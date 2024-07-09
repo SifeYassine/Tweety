69a99cd21caa8479b5c800feddf537cdd791a9ce
SifeYassine <sifeddineyassine1@gmail.com>
2024-07-09 15:51:35 +0100

    Create Sidebar

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <SideBar />
+</template>
+
+<style>
+body {
+  background-color: black;
+}
+</style>
+
+<script>
+// import { ref, onMounted } from "vue";
+// import axios from "axios";
+import "../src/style.css";
+import SideBar from "./components/SideBar.vue";
+
+export default {
+  name: "App",
+  components: {
+    SideBar,
+  },
+};
+</script>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,98 @@
+<template>
+  <!-- Sidebar section -->
+  <aside
+    class="fixed h-screen w-[15%] pt-5 pl-10 border-2 border-white text-white"
+  >
+    <!-- Logo -->
+    <img class="w-12 ml-[-10px] mb-2" src="../assets/logo.png" alt="Logo" />
+    <!-- List (unordered) of icons and corresponding texts in the sidebar -->
+    <ul>
+      <li>
+        <img
+          class="w-6 h-6 mr-2"
+          src="../assets/icons/home.svg"
+          alt="Home Icon"
+        />
+        <p>Home</p>
+      </li>
+      <li>
+        <img
+          class="w-6 h-6 mr-2"
+          src="../assets/icons/explore.svg"
+          alt="Explore Icon"
+        />
+        <p>Explore</p>
+      </li>
+      <li>
+        <img
+          class="w-6 h-6 mr-2"
+          src="../assets/icons/messages.svg"
+          alt="Messages Icon"
+        />
+        <p>Messages</p>
+      </li>
+      <li>
+        <img
+          class="w-6 h-6 mr-2"
+          src="../assets/icons/notifications.svg"
+          alt="Notifications Icon"
+        />
+        <p>Notifications</p>
+      </li>
+      <li>
+        <img
+          class="w-6 h-6 mr-2"
+          src="../assets/icons/bookmarks.svg"
+          alt="Bookmarks Icon"
+        />
+        <p>Bookmarks</p>
+      </li>
+      <li>
+        <img
+          class="w-6 h-6 mr-2"
+          src="../assets/icons/lists.svg"
+          alt="Lists Icon"
+        />
+        <p>Lists</p>
+      </li>
+      <!-- Mini profile image and text -->
+      <li>
+        <img
+          class="w-7 h-7 mr-2 border-[3px] border-opacity-80 border-white rounded-full"
+          src="../assets/Profile.png"
+          alt="Profile Image"
+        />
+        <p>Profile</p>
+      </li>
+      <li>
+        <img
+          class="w-6 h-6 mr-2"
+          src="../assets/icons/more.svg"
+          alt="More Icon"
+        />
+        <p>More</p>
+      </li>
+    </ul>
+
+    <!-- Tweet button -->
+    <button
+      class="bg-blue-500 text-white w-[90%] h-10 border-none rounded-full mt-5 ml-[-9px]"
+    >
+      Tweet
+    </button>
+  </aside>
+</template>
+
+<style>
+li {
+  display: flex;
+  align-items: center;
+  padding: 12px 0;
+}
+</style>
+
+<script>
+export default {
+  name: "SideBar",
+};
+</script>
